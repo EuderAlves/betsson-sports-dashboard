206a1c6a957c186cad3b0b7a8b13843859ddbdf6
Euder Alves <euder.alv@gmail.com>
2025-01-16 21:11:37 -0300

    start e criacao de strutura

---
+++ sports-dashboard/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'stats',
+    loadChildren: () =>
+      import('./features/stats/stats.module').then((m) => m.StatsModule),
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
